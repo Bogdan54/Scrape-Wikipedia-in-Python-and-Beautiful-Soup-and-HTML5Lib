164d77ea52972bc0e73deec484767b3b77b1bae5
Bogdan Căpitănescu <108011509+Bogdan54@users.noreply.github.com>
2023-05-03 09:01:33 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,22 +1,29 @@
+import tkinter as tk
 import subprocess
 
-while True:
-    print("""
-    Choose an option:
-    1. Search Wikipedia by search term
-    2. Search Wikipedia by URL
-    3. Exit
-    """)
-    choice = input("Enter choice: ")
+root = tk.Tk()
+root.title("Wiki Search")
 
-    if choice == '1':
-        search_term = input("Enter search term: ")
-        subprocess.call(["python", "wiki.py", "--search", search_term])
-    elif choice == '2':
-        url = input("Enter URL: ")
-        subprocess.call(["python", "wiki.py", "--url", url])
-    elif choice == '3':
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid choice. Please try again.")
+url_label = tk.Label(root, text="URL:")
+url_label.grid(row=0, column=0, padx=5, pady=5)
+
+url_entry = tk.Entry(root)
+url_entry.grid(row=0, column=1, padx=5, pady=5)
+
+search_label = tk.Label(root, text="Search:")
+search_label.grid(row=1, column=0, padx=5, pady=5)
+
+search_entry = tk.Entry(root)
+search_entry.grid(row=1, column=1, padx=5, pady=5)
+
+def search_wiki():
+    url = url_entry.get()
+    search = search_entry.get()
+
+    command = f"python wiki.py --url {url} --search '{search}'"
+    subprocess.call(command, shell=True)
+
+search_button = tk.Button(root, text="Search", command=search_wiki)
+search_button.grid(row=2, column=1, padx=5, pady=5)
+
+root.mainloop()
